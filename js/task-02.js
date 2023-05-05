7782a124fd7fbd2e5656b47908e190887ad93314
kuchmiy-v <wrdvrk@gmail.com>
2023-05-05 10:04:36 +0300

    task-2 rozpylennya

--- js/task-02.js
+++ js/task-02.js
@@ -8,24 +8,34 @@ const ingredients = [
 ];
 
 
-const fragment = document.createDocumentFragment();
 const ingredientsList = document.getElementById("ingredients");
 
 const listContent = ingredients
   .map((ingredient) => {const listItem = document.createElement("li");
     listItem.textContent = ingredient;
     listItem.classList.add("item");
-    fragment.appendChild(listItem);
-    // ingredientsList.appendChild(listItem);
-  })
-  .join("");
-
-  ingredientsList.appendChild(fragment);
-  
-// document.body.insertAdjacentHTML("beforeend", ingredientsList);
+    return listItem;
+  });
+ 
+  ingredientsList.append(...listContent);
 
 
 //=============================================================
+
+// const fragment = document.createDocumentFragment();
+// const ingredientsList = document.getElementById("ingredients");
+
+// const listContent = ingredients
+//   .map((ingredient) => {const listItem = document.createElement("li");
+//     listItem.textContent = ingredient;
+//     listItem.classList.add("item");
+//     fragment.appendChild(listItem);
+//   })
+//   .join("");
+
+//   ingredientsList.appendChild(fragment);
+
+//==============================================================
 
 // const listContent = ingredients
 //   .map((ingredient) => `<li class="item">${ingredient}</li>`)
